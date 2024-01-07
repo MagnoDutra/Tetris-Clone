1c11f38d8376faee376b23f8af897fac4bf0986f
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-07 14:37:42 -0300

    first commit

---
+++ Assets/Scripts/Core/Board.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private Transform m_emptyCell;
+    [SerializeField] private int m_height = 30;
+    [SerializeField] private int m_width = 10;
+    [SerializeField] private int m_header = 8;
+
+    Transform[,] m_grid;
+    private void Awake()
+    {
+        m_grid = new Transform[m_width, m_height];
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DrawEmptyCells();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DrawEmptyCells()
+    {
+        if (m_emptyCell != null)
+        {
+            for (int y = 0; y < m_height - m_header; y++)
+            {
+                for (int x = 0; x < m_width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate(m_emptyCell, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                    clone.name = $"Board Space ( x = {x.ToString()}, y = {y.ToString()}";
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("WARNING! Please assign the emptyCell object");
+        }
+    }
+}
