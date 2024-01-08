ea677b756ebe853d3f65218f29eaf61a836f50d3
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-07 23:32:59 -0300

    Criado funcoes helpers

--- Assets/Scripts/Managers/Spawner.cs
+++ Assets/Scripts/Managers/Spawner.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Shape[] m_allShapes;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,35 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Shape SpawnShape()
+    {
+        Shape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+        if (shape)
+        {
+            return shape;
+        }
+        else
+        {
+            Debug.LogWarning("WARNING! Invalid shape in spawner!");
+            return null;
+        }
+    }
+
+    private Shape GetRandomShape()
+    {
+        int i = Random.Range(0, m_allShapes.Length);
+
+        if (m_allShapes[i])
+        {
+            return m_allShapes[i];
+        }
+        else
+        {
+            Debug.LogWarning("Warning! Invalid shape!");
+            return null;
+        }
     }
 }
