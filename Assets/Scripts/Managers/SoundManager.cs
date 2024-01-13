507d41ed610b02316d3d4b24240abdae2f7fec87
MagnoDutra <jmagno.dutra@hotmail.com>
2024-01-13 12:23:37 -0300

    Icone muda na UI quando som e fx ligam e desligam

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -26,6 +26,9 @@ public class SoundManager : MonoBehaviour
     public AudioClip[] m_vocalClips;
     public AudioClip m_gameOverVocalClip;
 
+    public IconToggle m_musicIconToggle;
+    public IconToggle m_fxIconToggle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -81,10 +84,20 @@ public class SoundManager : MonoBehaviour
     {
         m_musicEnabled = !m_musicEnabled;
         UpdateMusic();
+
+        if (m_musicIconToggle)
+        {
+            m_musicIconToggle.ToggleIcon(m_musicEnabled);
+        }
     }
 
     public void ToggleFX()
     {
         m_fxEnabled = !m_fxEnabled;
+
+        if (m_fxIconToggle)
+        {
+            m_fxIconToggle.ToggleIcon(m_fxEnabled);
+        }
     }
 }
